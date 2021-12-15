bc39183b33e6dc37c69ced530353d4621e6e331a
KazanskiiPN <fokkoo@yandex.ru>
2021-12-15 22:56:23 +0300

    internet permition is added. the program is not working yet

--- app/src/main/java/com/example/matirialtry2/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/matirialtry2/ui/main/PictureOfTheDayFragment.kt
@@ -7,8 +7,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import coil.api.load
 import com.example.matirialtry2.R
 
@@ -33,7 +31,7 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dailyImageView = view.findViewById(R.id.image_view_nasa_image)
+        dailyImageView = view.findViewById(R.id.imageView)
     }
 
     private fun renderData(dailyImage: PictureOfTheDayData) {
@@ -60,4 +58,8 @@ class PictureOfTheDayFragment : Fragment() {
         }
     }
 
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+        private var isMain = true
+    }
 }
