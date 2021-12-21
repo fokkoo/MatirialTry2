1ecebe59c560a6e052ee274857ea42d9bd748a2f
KazanskiiPN <fokkoo@yandex.ru>
2021-12-21 16:50:24 +0300

    On my way

---
+++ app/src/main/java/com/example/matirialtry2/ui/main/UniverseFragment.kt
@@ -0,0 +1,58 @@
+package com.example.matirialtry2.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.matirialtry2.R
+import com.google.android.material.tabs.TabLayout
+
+
+class UniverseFragment : Fragment() {
+
+    private lateinit var viewPager: ViewPager2
+    p private lateinit var tabLayout: TabLayout
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_universe, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewPager = view.findViewById(R.id.view_pager_universe)
+        val adapter = UniverseStatePagerAdapter(this)
+
+
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment UniverseFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            UniverseFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
